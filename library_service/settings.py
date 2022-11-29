51d09ff96e230c6acc88a492a75ad18194c722fd
Alexander <alexzubkoff123@gmail.com>
2022-11-29 16:42:06 +0200

    Rsolved conf, added user app to settings and urls

--- library_service/settings.py
+++ library_service/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "book",
     "rest_framework",
-    "api",
+    "user",
 ]
 
 MIDDLEWARE = [
--- library_service/urls.py
+++ library_service/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/book/", include("book.urls", namespace="book")),
-    path("api/user/", include("user.urls", namespace="user"))
+    path("api/user/", include("user.urls", namespace="user")),
 ]
