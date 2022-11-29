50d16dc47f587e29b303597836a57217dbcf78e2
mltdll <ilya.vyplavin@gmail.com>
2022-11-29 14:35:34 +0200

    Set default authentication to JWT

--- user/tests/test_user_api.py
+++ user/tests/test_user_api.py
@@ -107,7 +107,7 @@ class PublicUserApiTests(TestCase):
         """Test that authentication is required for users"""
         res = self.client.get(ME_URL)
 
-        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
 
 class PrivateUserApiTests(TestCase):
