9ffe252460f1484ed9ec696069cc56fe639da5a2
Olia Trofymchuk <oliatrofymchuk@knu.ua>
2022-11-30 10:22:33 +0200

    Add url to the project urls

--- library_service/urls.py
+++ library_service/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-
+    path("api/borrowing/", include("borrowing.urls", namespace="borrowing")),
     path("api/book/", include("book.urls", namespace="book")),
     path("api/user/", include("user.urls", namespace="user")),
 ]
