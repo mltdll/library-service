7c4c5ededea61feacb5cb9d777fb77d566d9dbdf
Artem <Artemvyhovanec@gmail.com>
2022-11-30 03:47:21 +0200

    add CRUD for borrowing model

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-# Create your views here.
+from borrowing.models import Borrowing
+from borrowing.serializers import BorrowSerializer, BorrowDetailSerializer
+
+
+class BorrowViewSet(viewsets.ModelViewSet):
+    queryset = Borrowing.objects.all().prefetch_related("book")
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
+    def get_queryset(self):
+        queryset = self.queryset
+
+        if self.request.user.is_staff:
+            return queryset
+
+        return queryset.filter(user=self.request.user.id)
+
+    def get_serializer_class(self):
+        if self.action in ["list", "retrieve"]:
+            return BorrowDetailSerializer
+
+        return BorrowSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
