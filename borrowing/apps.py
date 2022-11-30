9bcb1406595f2e5047e9bf828b01c9ad23d953e4
mltdll <ilya.vyplavin@gmail.com>
2022-11-30 22:07:29 +0200

    Implement setup of the schedules for `borrowing`

--- borrowing/apps.py
+++ borrowing/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class BorrowingConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "borrowing"
+
+    def ready(self):
+        from .scheduled_tasks import schedule_tasks
+
+        # Schedule all the necessary tasks at the startup of the project
+        schedule_tasks()
