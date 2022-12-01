1678b5f12c8a89ba7e8a4a88487bd88f1ec79e3a
Artem <Artemvyhovanec@gmail.com>
2022-12-01 11:09:41 +0200

    Problems resolved

--- library_service/urls.py
+++ library_service/urls.py
@@ -28,7 +28,6 @@ urlpatterns = [
     path("api/borrow/", include("borrowing.urls", namespace="borrowing")),
     path("api/payment/", include("payment.urls", namespace="payment")),
     path("__debug__/", include("debug_toolbar.urls")),
-    path("__debug__/", include("debug_toolbar.urls")),
     path("api/doc/", SpectacularAPIView.as_view(), name="schema"),
     path(
         "api/doc/swagger/",
